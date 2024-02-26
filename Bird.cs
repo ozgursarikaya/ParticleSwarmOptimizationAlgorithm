ccc5d2ae8255dcf655d35a3881f95c7e32b64f4e
Özgür Sarıkaya <ozgursarikaya@gmail.com>
2024-02-27 01:21:45 +0300

    Add project files.

---
+++ Bird.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSwarmOptimizationAlgorithm
+{
+    public class Bird
+    {
+        public double[] Position { get; set; }
+        public double[] Velocity { get; set; }
+        public double[] BestPosition { get; set; }
+        public double BestFitness { get; set; }
+
+        public Bird(int dimension, double minX, double maxX, Random random)
+        {
+            Position = new double[dimension];
+            Velocity = new double[dimension];
+            BestPosition = new double[dimension];
+            BestFitness = double.MaxValue;
+
+            for (int i = 0; i < dimension; i++)
+            {
+                Position[i] = random.NextDouble() * (maxX - minX) + minX;
+                Velocity[i] = random.NextDouble() * (maxX - minX) - (maxX - minX) / 2;
+            }
+
+            Array.Copy(Position, BestPosition, dimension);
+            BestFitness = CalculateFitness(Position);
+        }
+
+        public double CalculateFitness(double[] position)
+        {
+            return position[0] * position[0] + position[1] * position[1]; // x^2 + y^2
+        }
+
+        public void UpdateBestPosition()
+        {
+            double currentFitness = CalculateFitness(Position);
+            if (currentFitness < BestFitness)
+            {
+                BestFitness = currentFitness;
+                Array.Copy(Position, BestPosition, Position.Length);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using ParticleSwarmOptimizationAlgorithm;
+
+Random random = new Random();
+double minX = -10.0;
+double maxX = 10.0;
+int dimension = 2;
+int swarmSize = 30;
+int maxIterations = 100;
+
+Bird[] swarm = new Bird[swarmSize];
+
+for (int i = 0; i < swarmSize; i++)
+{
+    swarm[i] = new Bird(dimension, minX, maxX, random);
+}
+
+double[] globalBestPosition = new double[dimension];
+double globalBestFitness = double.MaxValue;
+
+for (int iteration = 0; iteration < maxIterations; iteration++)
+{
+    foreach (Bird bird in swarm)
+    {
+        for (int i = 0; i < dimension; i++)
+        {
+            double r1 = random.NextDouble();
+            double r2 = random.NextDouble();
+            bird.Velocity[i] = bird.Velocity[i] * 0.5 +
+                                r1 * 2.0 * (bird.BestPosition[i] - bird.Position[i]) +
+                                r2 * 2.0 * (globalBestPosition[i] - bird.Position[i]);
+
+            bird.Position[i] += bird.Velocity[i];
+
+            if (bird.Position[i] < minX)
+            {
+                bird.Position[i] = minX;
+            }
+            else if (bird.Position[i] > maxX)
+            {
+                bird.Position[i] = maxX;
+            }
+        }
+
+        bird.UpdateBestPosition();
+        if (bird.BestFitness < globalBestFitness)
+        {
+            globalBestFitness = bird.BestFitness;
+            Array.Copy(bird.BestPosition, globalBestPosition, dimension);
+        }
+    }
+
+    Console.WriteLine($"Iteration: {iteration}, Best fitness: {globalBestFitness}");
+}
+
+Console.WriteLine("Global Best Position: [" + string.Join(", ", globalBestPosition) + "]");
+Console.WriteLine("Global Best Fitness: " + globalBestFitness);
